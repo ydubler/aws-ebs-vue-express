a0e40edcc2f747db45402dab0c9b420cdcb41246
Yuri D Dubler <YuriDubler@Yuris-MacBook-Pro.local>
2021-03-05 10:22:03 -0700

    Added .vue files and a component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template>
-  <div id="app" class="example">{{ msg }}</div>
+  <div id="app" class="example">
+    <div>{{ msg }} {{ Math.random() }}</div>
+    <br/>
+    <div class="drp">
+      <date-range-picker
+        v-model="dateRange"
+        :ranges="ranges"
+        :auto-apply="autoapply"
+        opens="inline"
+      ></date-range-picker>
+    </div>
+  </div>
 </template>
 
 <script>
+import DateRangePicker from 'vue2-daterange-picker';
+import 'vue2-daterange-picker/dist/vue2-daterange-picker.css'
+
 export default {
   data () {
     return {
         name: 'app',
-        msg: 'Hello world!'
+        msg: 'Hello world!!!!',
+        dateRange: {
+          startDate: '2019-12-26',
+          endDate: '2022-12-28',
+        },
+        ranges:false,
+        autoapply:true,
+
     }
+  },
+  components: {
+    DateRangePicker
+  },
+  methods: {
+      dateFormat (classes, date) {
+        if (!classes.disabled) {
+          classes.disabled = date.getTime() > new Date()
+        }
+        return classes
+      }
   }
 }
 </script>
 
 <style>
 .example {
-  color: red;
+  color: black;
+}
+.drp{
+width:100%;
+text-align: center;
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import App from './App.vue';
-
 var vueView = new Vue({
   el: "#app",
   render: h => h(App),
